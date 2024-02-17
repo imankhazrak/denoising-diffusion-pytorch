94248be717af0696375cd07dc7275e98f2d570f3
Iman Khazrak <iman.khazrak.ik@gmail.com>
2024-02-17 00:42:09 -0500

    modify image address for server

--- my.py
+++ my.py
@@ -10,7 +10,7 @@ model = Unet(
 diffusion = GaussianDiffusion(
     model,
     image_size = 128,
-    timesteps = 1000,           # number of steps
+    timesteps = 10000,           # number of steps
     sampling_timesteps = 250    # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
 )
 
@@ -19,7 +19,7 @@ trainer = Trainer(
     r'/home/ikhazra/denoising-diffusion-pytorch/images/daisy',
     train_batch_size = 8,
     train_lr = 8e-5,
-    train_num_steps = 7,         # total training steps
+    train_num_steps = 7000,         # total training steps
     gradient_accumulate_every = 2,    # gradient accumulation steps
     ema_decay = 0.995,                # exponential moving average decay
     amp = True,                       # turn on mixed precision
@@ -27,3 +27,4 @@ trainer = Trainer(
 )
 
 trainer.train()
+
